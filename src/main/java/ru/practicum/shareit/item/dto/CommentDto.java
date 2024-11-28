09674bdfa66538283c94bb9ce5abb637c75f8137
lazyabrikos <vovager2003@yandex.ru>
2024-11-28 04:18:30 +0300

    Fix Codestyle

--- src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,8 +1,9 @@
 package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
-import ru.practicum.shareit.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
